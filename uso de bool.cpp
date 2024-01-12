b0b447b8a42b724132416bfc2f80b8078a8ef1b8
alexduran30 <155786215+alexduran30@users.noreply.github.com>
2024-01-12 19:35:03 -0400

    Add files via upload

---
+++ uso de bool.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char *argv[]) {
+	//declaracion uso de bool
+	bool b0;
+	b0=true;
+	cout<<"variable con valor TRUE:"<<b0<<endl;
+	b0=false;
+	cout<<"variable con valor FALSE:"<<b0<<endl;
+	//asignacion de valores numericos
+	//si es cero es falso, si es diferente de cero es verdadero
+	b0=0; //el valor se vera como falso
+	cout<<"variable con valor 0:"<<b0<<endl;
+	b0=-100; //el valor se vera como true.
+	cout<<"variable con valor -100:"<<b0<<endl;
+	return 0;
+}
