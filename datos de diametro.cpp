5d7b84a0891d0d6aa0871ff12e2a667a25f2c996
alexduran30 <155786215+alexduran30@users.noreply.github.com>
2024-01-12 21:58:30 -0400

    Add files via upload

---
+++ datos de diametro.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+//#define PI 3.1416
+
+
+int main(int argc, char *argv[]) {
+	const float PI = 3.1416; //definimos una constante llamada pi 
+	double diametro, circunferencia;
+	circunferencia=40;
+	diametro=circunferencia/PI;
+	cout<<"el diametrode la circunferencia=40 : "  <<diametro;//<<endl;
+	//PI=0// error por asignacion a una variable de solo lectura
+	return 0;
+}
+
